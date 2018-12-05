776de00d35893f6a0792cf951eb7fc71af68a693
Monique <moniquechan1220@gmail.com>
2018-12-05 11:28:58 +0800

    [Monique] Add connection between react and redux.

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,7 +1,12 @@
-const initialState = { count: 0 };
+const initialState = {
+  counterSum: 0
+};
 
 export default (state = initialState, { type, payload }) => {
   switch (type) {
+    case "COUNTERSUM":
+      console.log(payload)
+      return { counterSum: state.counterSum + payload };
     default:
       return state;
   }
